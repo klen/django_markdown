a42ad4d9a75e114afb7ce7cdec8f6cd06031c0d8
Kirill Klenov <horneds@gmail.com>
2011-03-07 11:41:52 +0300

    varsion 0.0.7

--- django_markdown/__init__.py
+++ django_markdown/__init__.py
@@ -1,2 +1,6 @@
-__version__ = VERSION = '0.0.6'
+VERSION_INFO = (0, 0, 7)
+
+__version__ = VERSION = '0.0.7'
 __project__ = PROJECT = 'django-markdown'
+__author__ = AUTHOR = "Kirill Klenov <horneds@gmail.com>"
+__license__ = LICENSE = "GNU LGPL"
