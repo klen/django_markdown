a8d0766a9e58060a3ab98d6e28aa43c01be9a4fd
art-zhitnik <art.zhitnik@gmail.com>
2013-07-02 16:00:39 +1000

    Avoiding "TypeError: render_to_string() got an unexpected keyword argument 'content'" on Django 1.4.1

--- django_markdown/views.py
+++ django_markdown/views.py
@@ -1,7 +1,9 @@
-from django.views.generic.simple import direct_to_template
-
+from django.conf import settings
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def preview(request):
-    return direct_to_template(
-            request, 'django_markdown/preview.html',
-            content=request.REQUEST.get('data', 'No content posted'))
+    media_or_static = settings.STATIC_URL or settings.MEDIA_URL
+    css = getattr(settings, 'DJANGO_MARKDOWN_STYLE', media_or_static + 'django_markdown/preview.css')
+    content=request.REQUEST.get('data', 'No content posted')
+    return  render_to_response('django_markdown/preview.html', locals(), context_instance=RequestContext(request))
