23660e797b2bda2974be18b64933227407d9d2c3
dfeinzeig <feinzeig@gmail.com>
2014-05-08 21:41:16 -0400

    protect preview url for staff only
    otherwise preview is accessible to all

--- django_markdown/urls.py
+++ django_markdown/urls.py
@@ -1,9 +1,11 @@
 """ Define preview URL. """
 
 from django.conf.urls import patterns, url
+from django.contrib.auth.decorators import user_passes_test
 
 from django_markdown.views import preview
 
+staff_required = user_passes_test(lambda user: user.is_staff)
 
 urlpatterns = patterns(
-    '', url('preview/$', preview, name='django_markdown_preview'))
+    '', url('preview/$', staff_required(preview), name='django_markdown_preview'))
