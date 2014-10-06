22b5bdd534ff35ebfb0a5763f6e2b82e098b560e
Kirill Klenov <horneds@gmail.com>
2014-10-06 21:11:19 +0100

    Bump version: 0.7.0 → 0.7.1

--- django_markdown/__init__.py
+++ django_markdown/__init__.py
@@ -1,6 +1,6 @@
 """ Django-Markdown supports markdown in Django. """
 
-__version__ = "0.7.0"
+__version__ = "0.7.1"
 __project__ = "django-markdown"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "GNU LGPL"
