1739f087ccebb2b6e084bbbd94e3928a3d699af6
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2014-12-10 20:50:54 -0600

    backward compatible way of checking if app is installed

--- django_markdown/templatetags/django_markdown_static.py
+++ django_markdown/templatetags/django_markdown_static.py
@@ -6,11 +6,24 @@ register = Library()
 _static = None
 
 
+def _is_installed(app):
+    """Workaround for checking whether app is installed that is compatable with
+    django 1.7 approach as well as providing backward compatability with
+    versions <1.7
+    """
+    try:
+        from django.apps import apps
+    except ImportError:
+        from django.conf import settings
+        return app in settings.INSTALLED_APPS
+    else:
+        return apps.is_installed(app)
+
 @register.simple_tag
 def static(path):
     global _static
     if _static is None:
-        if 'django.contrib.staticfiles' in settings.INSTALLED_APPS:
+        if _is_installed('django.contrib.staticfiles'):
             from django.contrib.staticfiles.templatetags.staticfiles import static as _static
         else:
             from django.templatetags.static import static as _static
