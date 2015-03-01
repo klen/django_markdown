caf5b5af07421c4e1ef0291e4e87033bf2426786
Alkalit <lyoffa412@gmail.com>
2015-03-01 14:02:27 +0500

    Refactoring of views test cases.

--- django_markdown/tests.py
+++ django_markdown/tests.py
@@ -5,6 +5,8 @@ from django_markdown.templatetags.django_markdown import (
     markdown as markdown_tag
 )
 
+from django.contrib.auth import models
+
 
 class DjangoMarkdownTagsTest(TestCase):
 
@@ -27,38 +29,59 @@ class DjangoMarkdownUtilsTest(TestCase):
         self.assertEqual(html, expected)
 
 
+# TODO status code check
 class DjangoMarkdownViewsTest(TestCase):
 
-    def test_preview_view(self):
+    def setUp(self):
+
+        self.data = {'data': "# header \n *test*"}
+
+    def test_preview_get_empty_request(self):
 
         response = self.client.get('/markdown/preview/')
+
         self.assertContains(response, 'No content posted')
         self.assertContains(response, 'preview.css')
 
-        response = self.client.get('/markdown/preview/', data=dict(
-            data="# header \n *test*"))
+    def test_preview_get_markdown_request(self):
+
+        response = self.client.get('/markdown/preview/', data=self.data)
+
         self.assertContains(response, '<h1>header</h1>')
 
-        response = self.client.post('/markdown/preview/', data=dict(
-            data="# header \n *test*"))
+    def test_preview_post_markdown_request(self):
+
+        data = {'data': "# header \n *test*"}
+
+        response = self.client.post('/markdown/preview/', data=self.data)
+
         self.assertContains(response, '<h1>header</h1>')
 
+    def test_preview_MARKDOWN_PROTECT_PREVIEW(self):
+        # monkey patching
         from . import settings
         settings.MARKDOWN_PROTECT_PREVIEW = True
-        response = self.client.get(
-            '/markdown/preview/',
-            data=dict(
-                data="# header \n *test*"))
+
+        response = self.client.get('/markdown/preview/', data=self.data)
+
         self.assertEqual(response.status_code, 302)
 
-        from django.contrib.auth import models
-        user = models.User.objects.create(username='test', is_staff=True)
-        user.set_password('test')
-        user.save()
-        self.client.login(username='test', password='test')
+        # for tests isolation reasons
+        settings.MARKDOWN_PROTECT_PREVIEW = False
 
-        response = self.client.get(
-            '/markdown/preview/',
-            data=dict(
-                data="# header \n *test*"))
+    def test_preview_get_markdown_for_admin_user_registered(self):
+
+        username = 'test'
+        password = 'test',
+
+        user = models.User.objects.create(
+            username=username,
+            password=password,
+            is_staff=True
+        )
+
+        self.client.login(username=username, password=password)
+
+        response = self.client.get('/markdown/preview/', data=self.data)
+
         self.assertContains(response, '<h1>header</h1>')
