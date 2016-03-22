934f59c211496f6bd94b6fb4639bcf59facb08c3
Ulises Quintanilla <ulisesqg@gmail.com>
2016-03-22 13:33:13 -0700

    Change urls format, url patterns is deprecated and will be removed in Django 1.10

--- django_markdown/urls.py
+++ django_markdown/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import patterns, url
 
 from .views import preview
 
-urlpatterns = patterns(
-    '', url('preview/$', preview, name='django_markdown_preview'))
+urlpatterns = [
+    url('preview/$', preview, name='django_markdown_preview'),
+]
