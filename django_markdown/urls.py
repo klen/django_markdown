b1d4fafa18628cfaf4ed284d20888c95d58fb52a
Slavik Svyrydiuk <svyrydiuk@gmail.com>
2016-07-23 08:13:46 +0200

    fix urls.py
    django.conf.urls.patterns will be depricated in django 1.10

--- django_markdown/urls.py
+++ django_markdown/urls.py
@@ -1,9 +1,18 @@
 """ Define preview URL. """
 
+from django import VERSION
 from django.conf.urls import url
 
 from .views import preview
 
-urlpatterns = [
-    url('preview/$', preview, name='django_markdown_preview')
-]
+
+if VERSION >= (1, 8):
+    urlpatterns = [
+        url('preview/$', preview, name='django_markdown_preview')
+    ]
+else:
+    # django <= 1.7 compatibility
+    from django.conf.urls import patterns
+    urlpatterns = patterns(
+        '', url('preview/$', preview, name='django_markdown_preview')
+    )
